18a9cc496343a43ff1287728649d0ebadb9bdcdd
udaybisht124 <bishtuday4668@gmail.coom>
2024-08-22 10:37:55 +0530

    changes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='w-full container flex flex-row bg-black'>
-       <div className='logo text-blue-500 font-bold text-xl '>iTask</div>
+       <div className='logo text-blue-500 font-bold text-xl my-auto '>iTask</div>
+       <div><img src="https://todolist.surajsingh.online/logo.ico" alt="" className='logoimage'  
+    /></div>
        <div className='text-blue-500 font-bold mt-7 right'><ul><li>Home</li>
        </ul></div>
     </div>
